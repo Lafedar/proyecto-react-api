31314fd214627c3ad5b52b84efced0658bfc6236
geroalles <geronimo.alles@lafedar.com>
2025-06-25 13:44:16 -0300

    usa rmenos alto de pantalla para scrollear

--- src/myRequests.jsx
+++ src/myRequests.jsx
@@ -82,8 +82,7 @@ function MyRequests() {
 
             {!loading && (
                 <div className="w-full mx-auto mt-30 md:mb-0">
-                    <div className="md:max-h-[calc(100vh-200px)] md:overflow-y-auto px-4 hide-scrollbar">
-
+                    <div className="max-h-[calc(100vh-180px)] overflow-y-auto px-4 hide-scrollbar">
                         <div className="flex flex-col items-center gap-3" style={{ minWidth: '350px' }}>
 
                             {requests.length === 0 ? (
