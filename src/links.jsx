35c3a6eb1cdf95e26ed71a123af07e5d75f2af48
geroalles <geronimo.alles@lafedar.com>
2025-07-03 16:43:23 -0300

    mantener la misma pestaña al salir desde links

--- src/links.jsx
+++ src/links.jsx
@@ -88,22 +88,6 @@ function MedicationButton({ type = 'button', children, to }) {
     );
 }
 
-function AlmuerzosButton({ type = 'button', children, to }) {
-    const handleClick = () => {
-        if (to) {
-            window.open(to, '_blank', 'noopener,noreferrer'); //abre eñ link en otra pestaña de forma segura
-        }
-    };
-
-    return (
-        <button
-            type={type}
-            onClick={handleClick}
-            className="w-[300px] bg-blue-500 transition delay-700 duration-700 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
-            {children}
-        </button>
-    );
-}
 function MyRequestsButton({ type = 'button', children, to }) {
     const navigate = useNavigate();
 
@@ -131,7 +115,8 @@ function ExitButton({ type = 'button', children, to }) {
         sessionStorage.removeItem('authToken');
         sessionStorage.removeItem('sessionKey');
         if (to) {
-            window.open(to, 'noopener,noreferrer'); //abre eñ link en otra pestaña de forma segura
+            window.location.href = to;
+
         }
     };
 
