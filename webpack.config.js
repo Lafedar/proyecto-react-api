bb76f59c12afabe1ab483ccc007aefed2dac907d
geroalles <geronimo.alles@lafedar.com>
2025-07-21 14:34:07 -0300

    copiar favicon.ico al hacer el build del proyecto en netlify

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: path.resolve(__dirname, '_redirects'), to: '' },
+        { from: 'public/favicon.ico', to: 'favicon.ico' },
       
       ],
     }),
